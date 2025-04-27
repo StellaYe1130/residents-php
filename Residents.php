87e34cf935130cadc3122c96dcd5e535a10efe84
StellaYe1130 <xinyanye2004@gmail.com>
2025-04-27 20:29:03 +1000

    Add files via upload
    add php

---
+++ Residents.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json');
+
+$owners = [
+    [
+        "name" => "Stella",
+        "unit" => "807",
+        "email" => "stella@gmail.com"
+        "phone" => "0412345678"
+    ],
+    [
+        "name" => "Zimo",
+        "unit" => "805",
+        "email" => "Zimo@outlook.com"
+        "phone" => "0498763524"
+    ],
+    [
+        "name" => "Hiky",
+        "unit" => "803",
+        "email" => "Hiky@qq.com"
+        "phone" => "0475622713"
+    ]
+];
+
+echo json_encode($Residents);
+?>
